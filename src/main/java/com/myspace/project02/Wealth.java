b3143f28b2bacbe1a3d5c7ccc1ab72c4875ba309
pam01 <>
2020-06-12 08:14:55 +0000

    {/src/main/java/com/myspace/project02/Wealth.java}

--- src/main/java/com/myspace/project02/Wealth.java
+++ src/main/java/com/myspace/project02/Wealth.java
@@ -6,12 +6,35 @@ package com.myspace.project02;
 
 public class Wealth implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Wealth() {
-    }
+	@org.kie.api.definition.type.Label(value = "Salary")
+	private java.math.BigInteger salary;
+	@org.kie.api.definition.type.Label(value = "Status")
+	private java.lang.String status;
 
+	public Wealth() {
+	}
 
+	public java.math.BigInteger getSalary() {
+		return this.salary;
+	}
 
+	public void setSalary(java.math.BigInteger salary) {
+		this.salary = salary;
+	}
+
+	public java.lang.String getStatus() {
+		return this.status;
+	}
+
+	public void setStatus(java.lang.String status) {
+		this.status = status;
+	}
+
+	public Wealth(java.math.BigInteger salary, java.lang.String status) {
+		this.salary = salary;
+		this.status = status;
+	}
 
 }
